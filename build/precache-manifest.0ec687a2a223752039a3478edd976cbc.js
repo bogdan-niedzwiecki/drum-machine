b533c0fb3a38591291b838430bd0b578f39d4fa7
Bogdan Niedzwiecki <bogdan.niedzwiecki@possible.com>
2020-07-14 15:19:27 +0200

    build

---
+++ build/precache-manifest.0ec687a2a223752039a3478edd976cbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2074dd7a71eb5e48975c31c253bf5ae5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f137a9cd3a302e15d21",
+    "url": "/static/css/main.d1d9acfe.chunk.css"
+  },
+  {
+    "revision": "14d67f88eb14a86f4cbd",
+    "url": "/static/js/2.37830858.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.37830858.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f137a9cd3a302e15d21",
+    "url": "/static/js/main.28249af5.chunk.js"
+  },
+  {
+    "revision": "0bacb7dad2b1a1f0466b",
+    "url": "/static/js/runtime-main.9181ec70.js"
+  }
+]);
